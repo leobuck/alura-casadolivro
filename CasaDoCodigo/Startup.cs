463d847816f5205e40a48c09a6b0a6fc1984f9d1
leobuck <leobuck12@gmail.com>
2023-02-18 12:48:20 -0300

    Método EnsureCreated X Método Migrate

--- CasaDoCodigo/Startup.cs
+++ CasaDoCodigo/Startup.cs
@@ -60,7 +60,7 @@ namespace CasaDoCodigo
                     pattern: "{controller=Pedido}/{action=Carrossel}/{id?}");
             });
 
-            serviceProvider.GetService<ApplicationContext>().Database.EnsureCreated();
+            serviceProvider.GetService<ApplicationContext>().Database.Migrate();
         }
     }
 }
