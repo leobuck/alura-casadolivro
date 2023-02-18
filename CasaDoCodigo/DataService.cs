97f22c9449805e8808af4b5e266cff06659e5da9
leobuck <leobuck12@gmail.com>
2023-02-18 14:50:53 -0300

    DataService, Injeção de dependência

---
+++ CasaDoCodigo/DataService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CasaDoCodigo
+{
+    class DataService : IDataService
+    {
+        private readonly ApplicationContext context;
+
+        public DataService(ApplicationContext context)
+        {
+            this.context = context;
+        }
+
+        public void InicializarDB()
+        {
+            context.Database.Migrate();
+        }
+    }
+}
--- CasaDoCodigo/Startup.cs
+++ CasaDoCodigo/Startup.cs
@@ -31,6 +31,8 @@ namespace CasaDoCodigo
             services.AddDbContext<ApplicationContext>(options =>
                 options.UseSqlServer(connectionString)
             );
+
+            services.AddTransient<IDataService, DataService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -60,7 +62,7 @@ namespace CasaDoCodigo
                     pattern: "{controller=Pedido}/{action=Carrossel}/{id?}");
             });
 
-            serviceProvider.GetService<ApplicationContext>().Database.Migrate();
+            serviceProvider.GetService<DataService>().InicializarDB();
         }
     }
 }
