642b3318a0b05329040beb07db78a80bb9115543
leobuck <leobuck12@gmail.com>
2023-02-26 19:25:37 -0300

    Injetando o Cadastro na View e Redirecionando se Necessário

--- CasaDoCodigo/Controllers/PedidoController.cs
+++ CasaDoCodigo/Controllers/PedidoController.cs
@@ -24,7 +24,14 @@ namespace CasaDoCodigo.Controllers
 
         public IActionResult Cadastro()
         {
-            return View();
+            var pedido = pedidoRepository.GetPedido();
+
+            if (pedido == null)
+            {
+                return RedirectToAction("Carrossel");
+            }
+
+            return View(pedido.Cadastro);
         }
 
         public IActionResult Carrinho(string codigo)
